32acd6bb8f416f997939b207b49b8faf29b9ee19
hyouido <hyouido@gmail.com>
2021-06-11 22:54:11 +0900

    모바일 프로그래밍 프로젝트

--- app/src/main/java/com/example/sns_project/settingpage.java
+++ app/src/main/java/com/example/sns_project/settingpage.java
@@ -10,7 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
-public class frag4 extends Fragment {
+public class settingpage extends Fragment {
 
     private View view;
 
